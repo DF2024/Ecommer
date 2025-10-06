0be7fa62040e877877591737d8ee8679aa6f7e31
Dandefensor <ad79702@gmail.com>
2025-10-06 18:05:12 -0400

    Se agregaron mas endpoints

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,18 +1,25 @@
-from sqlmodel import SQLModel, Field, Optional 
+from sqlmodel import SQLModel, Field 
+from datetime import datetime
 from pydantic import EmailStr
+from typing import Optional
 
-
-class UserBase(SQLModel):
-    username : str = Field(Default= None)
-    email : EmailStr = Field(Defualt = None)
+class User(SQLModel, table = True):
+    id : int | None = Field(default = None, primary_key = True)
+    username : str = Field(default = None)
+    email : EmailStr = Field(default = None)
     hashed_password : str
-    age : int
-    
-class User(UserBase, table = True):
-    id : int = Optional[Field(default = None, primary_key = True)]
+    created_at : datetime = Field(default_factory=datetime.utcnow)
 
-class UserCreate(UserBase):
+class UserCreate(SQLModel):
+    username : str
+    email : EmailStr
     password : str
 
-class UserResponse(UserBase):
-    pass
+class UserLogin(SQLModel):
+    username : str
+    password : str
+
+class UserResponse(SQLModel):
+    id: int
+    username: str
+    email: EmailStr
--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,7 +1,60 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from models.users import UserCreate, User, UserResponse, UserLogin
+from sqlmodel import select, update, delete
+from config.db import SessionDep
+from auth import auth
 
 router = APIRouter()
 
-@router.get('/')
-async def ola():
-    return {'messege' : 'Hola desde el router'}
+@router.post('/users/register', response_model = UserResponse, tags = ['Users'])
+async def user_register(
+    user_data : UserCreate,
+    session : SessionDep,
+):
+    statement = select(User).where(User.username == user_data.username)
+    existing_user = session.exec(statement).first()
+    if existing_user:
+        raise HTTPException(status_code = 400, detail = "Usuarion Existente")
+
+    hashed_password = auth.hash_password(user_data.password)
+
+    new_user = User(
+        username = user_data.username,
+        email = user_data.email,
+        hashed_password = hashed_password,
+    )
+
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+
+    return {"message" : "Usuario Registrado con exito!"}
+
+@router.get("/users", response_model = list[User], tags = ['Users'])
+async def user_get(session : SessionDep):
+    statament = select(User)
+    result = session.exec(statament)
+    users = result.all()
+    return users
+
+@router.get("/users/{id_user}", response_model = User ,tags = ['Users'])
+async def get_user_id(id_user : int, session : SessionDep):
+    statament = select(User).where(User.id == id_user)
+    user = session.exec(statament).first()
+    return user
+
+@router.delete("/users/{id_user}", tags = ['Users'])
+async def delete_user(id_user : int, session : SessionDep):
+    user_db = session.get(User, id_user)
+
+
+    if not user_db:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND, 
+            detail = "Task doesn't exist"
+        )
+    
+    session.delete(user_db)
+    session.commit()
+    return {'message': 'Task deleted successfully', 
+            'deleted_task': user_db.dict()} 
